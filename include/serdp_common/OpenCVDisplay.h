cd4b16b3ed99f9bd94cedad5beeeed9f223fcc1f
Aaron Marburg <amarburg@uw.edu>
2020-06-18 15:21:34 -0700

    Cleanup code and adapt to upstream changes.

--- include/serdp_common/OpenCVDisplay.h
+++ include/serdp_common/OpenCVDisplay.h
@@ -12,7 +12,6 @@
 namespace serdp_common {
 
 using std::vector;
-class SerdpRecorder;
 
 class OpenCVDisplay {
 public:
@@ -20,14 +19,22 @@ public:
 
   bool setEnabled(bool e) { return _enabled = e; }
 
-  void showVideo(vector<cv::Mat> mats) {
-    if (_enabled)
-      _thread->send(std::bind(&OpenCVDisplay::implShowVideo, this, mats));
+  void showVideo(const cv::Mat &mat, const std::string &name = "Image") {
+    if (_enabled) {
+        vector<cv::Mat> vec;
+        vec.push_back(mat);
+      _thread->send(std::bind(&OpenCVDisplay::implShowVideo, this, vec, name));
+    }
   }
 
-  void showSonar(const std::shared_ptr<liboculus::SimplePingResult> &ping) {
+  void showVideo(vector<cv::Mat> mats, const std::string &name = "Image") {
     if (_enabled)
-      _thread->send(std::bind(&OpenCVDisplay::implShowSonar, this, ping));
+      _thread->send(std::bind(&OpenCVDisplay::implShowVideo, this, mats, name));
+  }
+
+  void showSonar(const liboculus::SimplePingResult &ping, const std::string &name = "Sonar") {
+    if (_enabled)
+      _thread->send(std::bind(&OpenCVDisplay::implShowSonar, this, ping, name));
   }
 
   float setPreviewScale(float scale) {
@@ -35,14 +42,13 @@ public:
     return _previewScale;
   }
 
-  cv::Mat
-  sonarPing2Img(const std::shared_ptr<liboculus::SimplePingResult> &ping);
+  //cv::Mat sonarPing2Img(const std::shared_ptr<liboculus::SimplePingResult> &ping);
 
 protected:
-  void implShowVideo(vector<cv::Mat> mats);
-  void resizeImage(const cv::Mat &rawImage, cv::Mat &scaledImage);
+  void implShowVideo(vector<cv::Mat> mats, const std::string &name = "Image");
+  void implShowSonar(const liboculus::SimplePingResult ping, const std::string &name = "Image");
 
-  void implShowSonar(const std::shared_ptr<liboculus::SimplePingResult> &ping);
+  void resizeImage(const cv::Mat &rawImage, cv::Mat &scaledImage);
 
 private:
   bool _enabled;
