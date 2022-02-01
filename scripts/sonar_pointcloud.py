ca12cc6b89ba4b94d09d69b5d552a074105526ee
Marc Micatka <micat001@uw.edu>
2022-02-01 11:54:51 -0800

    vectorized callback for about 50x speedup.

--- scripts/sonar_pointcloud.py
+++ scripts/sonar_pointcloud.py
@@ -12,20 +12,13 @@ from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 from sensor_msgs import point_cloud2
 
-# mappings between PointField types and numpy types
-type_mappings = [(PointField.INT8, np.dtype('int8')), (PointField.UINT8, np.dtype('uint8')), (PointField.INT16, np.dtype('int16')),
-                 (PointField.UINT16, np.dtype('uint16')), (PointField.INT32, np.dtype('int32')), (PointField.UINT32, np.dtype('uint32')),
-                 (PointField.FLOAT32, np.dtype('float32')), (PointField.FLOAT64, np.dtype('float64'))]
-pftype_to_nptype = dict(type_mappings)
-nptype_to_pftype = dict((nptype, pftype) for pftype, nptype in type_mappings)
-
 class SonarTranslator(object):
     def __init__(self):
         # NB: if queue_size is set, have to be sure buff_size is sufficiently large,
         #     since it can only discard messages that are fully in the buffer.
         # https://stackoverflow.com/questions/26415699/ros-subscriber-not-up-to-date
         # (200k didn't work, and sys.getsizeof doesn't return an accurate size of the whole object)
-        self.sub = rospy.Subscriber("sonar_image", SonarImage, self.callback, queue_size=1, buff_size=1000000)
+        self.sub = rospy.Subscriber("sonar_image", SonarImage, self.callback_fast, queue_size=1, buff_size=1000000)
         self.pub = rospy.Publisher("sonar_cloud", PointCloud2, queue_size=1)
 
         min_elev_deg = rospy.get_param("~min_elev_deg", 0)
@@ -118,10 +111,12 @@ class SonarTranslator(object):
         self.geometry = list(output)
 
     def callback(self, image_msg):
+
+        rospy.logdebug("Received new image, seq %d at %f" % (image_msg.header.seq, image_msg.header.stamp.to_sec()))
+
         header = Header()
         header = image_msg.header
 
-        # If specified, rewrite the frame in th
         frame_id = rospy.get_param("~frame_id", None)
         if frame_id:
             header.frame_id = frame_id
@@ -133,7 +128,7 @@ class SonarTranslator(object):
                   PointField('z', 8, PointField.FLOAT32, 1),
                   PointField('rgba', 12, PointField.UINT32, 1)]
 
-        # t0 = time.time()
+        t0 = time.time()
         nranges = len(image_msg.ranges)
         nangles = len(image_msg.azimuth_angles)
         npts = nranges * nangles
@@ -151,10 +146,51 @@ class SonarTranslator(object):
 
         # Construction of PointCloud2 cribbed from:
         # https://answers.ros.org/question/289576/understanding-the-bytes-in-a-pcl2-message/
-        # points = []
-        # return_idxs, = np.where(np.array([int(ii) for ii in image_msg.intensities]) > self.intensity_threshold)
-        # rospy.loginfo("{} (out of {}) points are above threshold".format(len(return_idxs), len(image_msg.intensities)))
-        start_time = time.time()
+        points = []
+        return_idxs, = np.where(np.array([int(ii) for ii in image_msg.intensities]) > self.intensity_threshold)
+        rospy.logdebug("{} (out of {}) points are above threshold".format(len(return_idxs), len(image_msg.intensities)))
+        for geometry in self.geometry:
+            pts = [[xx, yy, zz, self.intensity_lookup[aa-self.intensity_threshold] if aa > self.intensity_threshold else 0]
+                   for (xx, yy, zz), aa in zip(geometry, image_msg.intensities)]
+            points.extend(pts)
+
+        t1 = time.time()
+        cloud_msg = point_cloud2.create_cloud(header, fields, points)
+        dt0 = t1 - t0
+        dt1 = time.time() - t1
+
+        total_time = time.time()
+        self.pub.publish(cloud_msg)
+
+        rospy.loginfo(f"published pointcloud: npts = {npts}, Find Pts = {dt0:0.3f}, Convert to Cloud = {dt1:0.3f}. Total Time = {(total_time - t0):0.3f}")
+
+    def callback_fast(self, image_msg):
+        header = Header()
+        header = image_msg.header
+
+        # If specified, rewrite the frame in th
+        frame_id = rospy.get_param("~frame_id", None)
+        if frame_id:
+            header.frame_id = frame_id
+
+        if image_msg.data_size != 1:
+            raise Exception("NYI: mult-byte intensity data")
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
+                  PointField('y', 4, PointField.FLOAT32, 1),
+                  PointField('z', 8, PointField.FLOAT32, 1),
+                  PointField('rgba', 12, PointField.UINT32, 1)]
+
+        t0 = time.time()
+        nranges = len(image_msg.ranges)
+        nangles = len(image_msg.azimuth_angles)
+        npts = nranges * nangles
+
+        if self.geometry is None:
+            self.make_geometry(image_msg)
+        if self.intensity_lookup is None:
+            self.make_intensity_lookup()
+
+        # Construction of PointCloud2 from: https://gist.github.com/pgorczak/5c717baa44479fa064eb8d33ea4587e0
         intensities = np.frombuffer(image_msg.intensities, dtype=np.uint8)
         output_points = np.empty((nranges * nangles, 4))
         output_points[:, 0:3] = self.geometry_np
@@ -164,75 +200,27 @@ class SonarTranslator(object):
         dtype = np.float32
         itemsize = np.dtype(dtype).itemsize
         data = output_points.astype(dtype).tobytes()
-        # cloud_msg = PointCloud2(
-        #                         header=header,
-        #                         height=1,
-        #                         width=output_points.shape[0],
-        #                         is_dense=False,
-        #                         is_bigendian=False,
-        #                         fields=fields,
-        #                         point_step=(itemsize * 3),
-        #                         row_step=(itemsize * 4 * output_points.shape[0]),
-        #                         data=data
-        #                     )
-        t0 = time.time() - start_time        
-         
-        # start_time = time.time()
-        points = []                     
-        for geometry in self.geometry:
-            pts = [[xx, yy, zz, self.intensity_lookup[aa-self.intensity_threshold] if aa > self.intensity_threshold else 0]
-                   for (xx, yy, zz), aa in zip(geometry, image_msg.intensities)]
-            points.extend(pts)
 
-        # t1= time.time() - start_time
-
-        # t2 = time.time()
+        t1 = time.time()
+        cloud_msg = PointCloud2(
+                                header=header,
+                                height=1,
+                                width=output_points.shape[0],
+                                is_dense=False,
+                                is_bigendian=False,
+                                fields=fields,
+                                point_step=(itemsize * 4),
+                                row_step=(itemsize * 4 * output_points.shape[0]),
+                                data=data
+                            )
         
-
-        cloud_msg = point_cloud2.create_cloud(header, fields, points)
-        # start = time.time()
-        # test_msg = point_cloud(output_points, frame_id)
-        # rospy.loginfo(f'{(time.time() - start):.3f}')
-        # t2 = time.time() - start_time
-        
+        dt0 = t1 - t0
+        dt1 = time.time() - t1
+        total_time = time.time()
         self.pub.publish(cloud_msg)
-        # rospy.loginfo(f'Created Cloud (fast): {t0:.3f}s')
 
-def point_cloud(points, parent_frame):
-    """ Creates a point cloud message.
-    Args:
-        points: Nx4 array of xyz positions (m) and a colors
-        parent_frame: frame in which the point cloud is defined
-    Returns:
-        sensor_msgs/PointCloud2 message
-    """
-    ros_dtype = PointField.FLOAT32
-    dtype = np.float32
-    itemsize = np.dtype(dtype).itemsize
-    data = points.astype(dtype).tobytes()
+        rospy.loginfo(f"published pointcloud: npts = {npts}, Find Pts = {dt0:0.3f}, Convert to Cloud = {dt1:0.3f}. Total Time = {(total_time - t0):0.3f}")
 
-    # fields = [PointField(
-    #     name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-    #     for i, n in enumerate('xyza')]
-    fields = [PointField('x', 0, PointField.FLOAT32, 1),
-                  PointField('y', 4, PointField.FLOAT32, 1),
-                  PointField('z', 8, PointField.FLOAT32, 1),
-                  PointField('rgba', 12, PointField.UINT32, 1)]
-    
-
-    header = Header(frame_id=parent_frame, stamp=rospy.Time.now())
-
-    return PointCloud2(
-        header=header,
-        height=1,
-        width=points.shape[0],
-        is_dense=False,
-        is_bigendian=False,
-        fields=fields,
-        point_step=(itemsize * 7),
-        row_step=(itemsize * 7 * points.shape[0]),
-        data=data
-    )
 
 if __name__ == "__main__":
     rospy.init_node("sonar_pointcloud")
