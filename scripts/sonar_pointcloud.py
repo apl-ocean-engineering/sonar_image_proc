fixed callback_fast with log scaling

scripts/sonar_pointcloud.py
@@ -12,6 +12,8 @@ from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 from sensor_msgs import point_cloud2
 
+import ctypes
+
 
 class SonarTranslator(object):
 
@@ -28,9 +30,10 @@ class SonarTranslator(object):
         self.pub = rospy.Publisher("sonar_cloud", PointCloud2, queue_size=1)
 
         min_elev_deg = rospy.get_param("~min_elev_deg", 0)
-        max_elev_deg = rospy.get_param("~max_elev_deg", 20)
+        max_elev_deg = rospy.get_param("~max_elev_deg", 0)
         assert (max_elev_deg >= min_elev_deg)
-        elev_step_deg = rospy.get_param("~elev_step_deg", 10.0)
+        elev_step_deg = rospy.get_param("~elev_step_deg", 20.0)
+        self.use_log_scale = rospy.get_param("~log_scale", True)
         self.min_elev = np.radians(min_elev_deg)
         self.max_elev = np.radians(max_elev_deg)
         self.elev_step = np.radians(elev_step_deg)
@@ -39,7 +42,8 @@ class SonarTranslator(object):
                                     self.elev_step)
 
         # TODO: assert that this is in range of uint8? (Or, msg.data_size)
-        self.intensity_threshold = rospy.get_param("~intensity_threshold", 100)
+        self.intensity_threshold = rospy.get_param("~intensity_threshold",
+                                                   0.74)
 
         # x,y,z coordinates of points
         # [ELEVATION_IDX, INTENSITY_IDX, DIMENSION_IDX]
@@ -58,12 +62,12 @@ class SonarTranslator(object):
         self.intensity_lookup = [0 for _ in range(256)]
 
         for aa in range(256):
-            r, g, b, _ = cm.inferno(aa)
+            r, g, b, _ = cm.magma(aa)
             rr = int(255 * r)
             gg = int(255 * g)
             bb = int(255 * b)
-            rgba = struct.unpack('I', struct.pack('BBBB', bb, gg, rr, aa))[0]
-            self.intensity_lookup[aa] = rgba
+            rgb = struct.unpack('I', struct.pack('BBBB', bb, gg, rr, 255))[0]
+            self.intensity_lookup[aa] = rgb
 
         self.intensity_lookup_np = np.array(self.intensity_lookup)
 
@@ -138,13 +142,24 @@ class SonarTranslator(object):
             intensities = np.frombuffer(image_msg.intensities, dtype=np.uint8)
         if image_msg.data_size == 4:
             intensities = np.frombuffer(image_msg.intensities, dtype=np.uint32)
-            # intensities = np.log(intensities + 1e-6)
-            intensities = (intensities / 256).astype(np.uint8)
-        print(intensities.max(), intensities.mean())
+
+            if self.use_log_scale:
+                new_intensites = intensities + 1
+
+                max_value = np.iinfo(np.uint32).max
+                v = np.log(new_intensites) / np.log(max_value)
+                vmax = 1.0
+                v = (v - self.intensity_threshold) / (vmax -
+                                                      self.intensity_threshold)
+                v = np.clip(v, a_min=0.0, a_max=1.0)
+                intensities = (np.iinfo(np.uint8).max * v).astype(np.uint8)
+            else:
+                intensities = (intensities / 256).astype(np.uint8)
+
         # Construction of PointCloud2 cribbed from:
         # https://answers.ros.org/question/289576/understanding-the-bytes-in-a-pcl2-message/
         points = []
-
+        print(np.max(intensities), np.min(intensities))
         return_idxs, = np.where(
             np.array([int(ii)
                       for ii in intensities]) > self.intensity_threshold)
@@ -154,9 +169,8 @@ class SonarTranslator(object):
 
         for geometry in self.geometry:
             pts = [[
-                xx, yy, zz,
-                self.intensity_lookup_np[aa - self.intensity_threshold]
-                if aa > self.intensity_threshold else 0
+                xx, yy, zz, self.intensity_lookup_np[aa] if
+                aa > self.intensity_threshold else self.intensity_lookup_np[0]
             ] for (xx, yy, zz), aa in zip(geometry, intensities)]
             points.extend(pts)
 
@@ -188,9 +202,11 @@ class SonarTranslator(object):
             PointField('x', 0, PointField.FLOAT32, 1),
             PointField('y', 4, PointField.FLOAT32, 1),
             PointField('z', 8, PointField.FLOAT32, 1),
-            PointField('rgba', 12, PointField.UINT32, 1)
+            PointField('r', 12, PointField.FLOAT32, 1),
+            PointField('g', 16, PointField.FLOAT32, 1),
+            PointField('b', 20, PointField.FLOAT32, 1)
         ]
-        t0 = time.time()
+
         nranges = len(image_msg.ranges)
         nangles = len(image_msg.azimuth_angles)
         npts = nranges * nangles
@@ -198,63 +214,64 @@ class SonarTranslator(object):
         if self.geometry is None:
             self.make_geometry(image_msg)
 
-        if self.color_lookup is None:
-            self.make_color_lookup()
-
         if self.intensity_lookup is None:
             self.make_intensity_lookup()
+            self.make_color_lookup()
 
         # Construction of PointCloud2 from: https://gist.github.com/pgorczak/5c717baa44479fa064eb8d33ea4587e0
         if image_msg.data_size == 1:
             intensities = np.frombuffer(image_msg.intensities, dtype=np.uint8)
         if image_msg.data_size == 4:
             intensities = np.frombuffer(image_msg.intensities, dtype=np.uint32)
-            # intensities = np.log(intensities)
-            intensities = (intensities / 256).astype(np.uint8)
+            if self.use_log_scale:
+                new_intensites = intensities + 1
 
-        output_points = np.empty((len(self.elevations) * nranges * nangles, 4))
-        # expanded_intensities = np.repeat(intensities[..., np.newaxis],
-        #                                  3,
-        #                                  axis=1)
+                max_value = np.iinfo(np.uint32).max
+                v = np.log(new_intensites) / np.log(max_value)
+                vmax = 1.0
+                v = (v - self.intensity_threshold) / (vmax -
+                                                      self.intensity_threshold)
+
+                v = np.clip(v, a_min=0.0, a_max=1.0)
+                intensities = (np.iinfo(np.uint8).max * v).astype(np.uint8)
+            else:
+                intensities = (intensities / 256).astype(np.uint8)
+
+        t0 = time.time()
+        output_points = np.empty((len(self.elevations) * nranges * nangles, 6),
+                                 dtype=np.float32)
+        expanded_intensities = np.repeat(intensities[..., np.newaxis],
+                                         3,
+                                         axis=1)
 
         for i in range(len(self.elevations)):
-            points = np.empty((nranges * nangles, 4))
+            points = np.empty((nranges * nangles, 6))
             if points[:, 0:3].shape != self.geometry_np[i, :, :].shape:
                 break
             points[:, 0:3] = self.geometry_np[i, :, :]
-            # print(points.shape, intensities.shape, self.color_lookup.shape)
-            points[:, 3] = np.where(
-                intensities > self.intensity_threshold,
-                self.intensity_lookup_np[intensities -
-                                         self.intensity_threshold], 0)
-            # points[:, 3:] = np.where(
-            #     expanded_intensities > self.intensity_threshold,
-            #     self.intensity_lookup_np[expanded_intensities[:, 0]],
-            #     np.zeros((nranges * nangles, 3)))
+            temp = np.where(expanded_intensities > self.intensity_threshold,
+                            self.color_lookup[expanded_intensities[:, 0]],
+                            np.zeros((nranges * nangles, 3)))
+            points[:, 3:] = np.where(
+                expanded_intensities > self.intensity_threshold,
+                self.color_lookup[expanded_intensities[:, 0]],
+                np.zeros((nranges * nangles, 3)))
 
             step = i * (nranges * nangles)
             next_step = step + (nranges * nangles)
             output_points[step:next_step, :] = points
 
-        print(
-            'lookup',
-            hex(self.intensity_lookup_np[intensities[0] -
-                                         self.intensity_threshold]))
-        print(intensities.max(), intensities.mean())
-        dtype = np.float32
-        itemsize = np.dtype(dtype).itemsize
-        data = output_points.astype(dtype).tobytes()
         t1 = time.time()
+        N = len(output_points)
         cloud_msg = PointCloud2(header=header,
                                 height=1,
-                                width=output_points.shape[0],
-                                is_dense=False,
+                                width=N,
+                                is_dense=True,
                                 is_bigendian=False,
                                 fields=fields,
-                                point_step=(itemsize * 4),
-                                row_step=(itemsize * 4 *
-                                          output_points.shape[0]),
-                                data=data)
+                                point_step=24,
+                                row_step=24 * N,
+                                data=output_points.tostring())
 
         dt0 = t1 - t0
         dt1 = time.time() - t1