39b073678e8155007ec8de7eaadce728bbacfbad
Aaron Marburg <amarburg@uw.edu>
2022-07-11 21:47:42 -0700

    Refactored SonarImageMsgInterface for clarity.

--- ros/include/sonar_image_proc/sonar_image_msg_interface.h
+++ ros/include/sonar_image_proc/sonar_image_msg_interface.h
@@ -28,8 +28,8 @@ struct SonarImageMsgInterface
 
   // n.b. do_log_scale is a mode switch which causes the intensity_*
   // functions in this SonarImageMsgInterface use a log (db) scale
-  // **ONLY** for 32-bit data (functionality is unchanged for 8-
-  // and 16-bit data)
+  // for 32-bit data **ONLY**  -- functionality is unchanged for 8-
+  // and 16-bit data
   //
   // It maps a given 32-bit intensity value i to a fraction of the
   // full scale of a 32-bit uint in the interval [0.0, 1.0]
@@ -37,7 +37,10 @@ struct SonarImageMsgInterface
   //
   //     l = 10 * log( i / (2^32-1) )
   //
-  // Such that l is between 10*log(1/(2^32-1)) (approx -96) and 0.0
+  // Such that l is between
+  //      ~ -96 for i = 1
+  //          and
+  //        0.0 for i = UINT32_MAX
   //
   // The intensity_* functions create a linear map m() from [min_db, max_db]
   // to the full range of the returned type (e.g. uint8, uint16, etc),
@@ -90,98 +93,71 @@ struct SonarImageMsgInterface
 
   float verticalTanSquared() const { return _verticalTanSquared; }
 
-  // When the underlying data is 8-bit, this optimized version
-  // returns that exact value.
+  // When the underlying data is 8-bit, returns that exact value
+  // from the underlying data
   //
   // If the underlying data is 16-bit, it returns a scaled value.
   uint8_t intensity_uint8(const AzimuthRangeIndices &idx) const override {
-    const auto i = index(idx);
+
+    if (do_log_scale_ && (_ping->data_size == 4)) {
+      return intensity_float_log(idx) * UINT8_MAX;
+    }
 
     if (_ping->data_size == 1) {
-      return _ping->intensities[i];
+      return read_uint8(idx);
     } else if (_ping->data_size == 2) {
-      return intensity_uint16(idx) >> 8;
+      return read_uint16(idx) >> 8;
     } else if (_ping->data_size == 4) {
-      if (do_log_scale_) {
-        return intensity_float(idx) * UINT8_MAX;
-      } else {
-        return intensity_uint32(idx) >> 24;
-      }
+      return read_uint32(idx) >> 24;
     }
 
     return 0;
   }
 
   uint16_t intensity_uint16(const AzimuthRangeIndices &idx) const override {
-    const auto i = index(idx);
+
+    if (do_log_scale_ && (_ping->data_size == 4)) {
+      return intensity_float_log(idx) * UINT16_MAX;
+    }
 
     if (_ping->data_size == 1) {
-      return _ping->intensities[i];
+      return read_uint8(idx) << 8;
     } else if (_ping->data_size == 2) {
-      return (static_cast<uint16_t>(_ping->intensities[i]) |
-              (static_cast<uint16_t>(_ping->intensities[i + 1]) << 8));
+      return read_uint16(idx);
     } else if (_ping->data_size == 4) {
-      if (do_log_scale_) {
-        return intensity_float(idx) * UINT16_MAX;
-      } else {
-        return intensity_uint32(idx) >> 16;
-      }
+      return read_uint32(idx) >> 16;
     }
     return 0;
   }
 
   uint32_t intensity_uint32(const AzimuthRangeIndices &idx) const override {
-    const auto i = index(idx);
+
+    if (do_log_scale_ && (_ping->data_size == 4)) {
+      return intensity_float_log(idx) * UINT32_MAX;
+    }
 
     if (_ping->data_size == 1) {
-      return intensity_uint8(idx);
+      return read_uint8(idx) << 24;
     } else if (_ping->data_size == 2) {
-      return intensity_uint16(idx);
+      return read_uint16(idx) << 16;
     } else if (_ping->data_size == 4) {
-      if (do_log_scale_) {
-        return intensity_float(idx) * UINT32_MAX;
-      } else {
-        return intensity_uint32_nonlog(i);
-      }
-    }
-    return 0;
-  }
-
-  uint32_t intensity_uint32_nonlog(const size_t i) const {
-    if (_ping->data_size == 4) {
-      const uint32_t v =
-          (static_cast<uint32_t>(_ping->intensities[i]) |
-           (static_cast<uint32_t>(_ping->intensities[i + 1]) << 8) |
-           (static_cast<uint32_t>(_ping->intensities[i + 2]) << 16) |
-           (static_cast<uint32_t>(_ping->intensities[i + 3]) << 24));
-      return v;
+      return read_uint32(idx);
     }
     return 0;
   }
 
   float intensity_float(const AzimuthRangeIndices &idx) const override {
-    const auto i = index(idx);
 
-    // !! n.b.  Need to ensure these calls aren't circular!!
+    if (do_log_scale_ && (_ping->data_size == 4)) {
+      return intensity_float_log(idx);
+    }
+
     if (_ping->data_size == 1) {
-      return static_cast<float>(intensity_uint8(idx)) / UINT8_MAX;
+      return static_cast<float>(read_uint8(idx)) / UINT8_MAX;
     } else if (_ping->data_size == 2) {
-      // Data is stored LSB
-      return static_cast<float>(intensity_uint16(idx)) / UINT16_MAX;
+      return static_cast<float>(read_uint16(idx)) / UINT16_MAX;
     } else if (_ping->data_size == 4) {
-      if (do_log_scale_) {
-        const auto intensity = intensity_uint32_nonlog(i);
-        const auto v =
-            log(static_cast<float>(std::max((uint)1, intensity)) / UINT32_MAX) *
-            10; // dbm
-
-        const auto min_db =
-            (min_db_ == 0 ? log(1.0 / UINT32_MAX) * 10 : min_db_);
-
-        return std::min(1.0, std::max(0.0, (v - min_db) / range_db_));
-      } else {
-        return static_cast<float>(intensity_uint32_nonlog(i)) / UINT32_MAX;
-      }
+      return static_cast<float>(read_uint32(idx)) / UINT32_MAX;
     }
     return 0.0;
   }
@@ -195,6 +171,45 @@ protected:
     assert((_ping->data_size == 1) || (_ping->data_size == 2) ||
            (_ping->data_size == 4));
     return _ping->data_size * ((idx.range() * nBearings()) + idx.azimuth());
+  }
+
+  // "raw" read functions.  Assumes the data type has already been checked
+  uint32_t read_uint8(const AzimuthRangeIndices &idx) const {
+    assert(_ping->data_size == 1);
+    const auto i = index(idx);
+
+    return (_ping->intensities[i]);
+  }
+
+  uint32_t read_uint16(const AzimuthRangeIndices &idx) const {
+    assert(_ping->data_size == 2);
+    const auto i = index(idx);
+
+    return (static_cast<uint16_t>(_ping->intensities[i]) |
+            (static_cast<uint16_t>(_ping->intensities[i + 1]) << 8));
+  }
+
+  uint32_t read_uint32(const AzimuthRangeIndices &idx) const {
+    assert(_ping->data_size == 4);
+    const auto i = index(idx);
+
+    const uint32_t v =
+        (static_cast<uint32_t>(_ping->intensities[i]) |
+         (static_cast<uint32_t>(_ping->intensities[i + 1]) << 8) |
+         (static_cast<uint32_t>(_ping->intensities[i + 2]) << 16) |
+         (static_cast<uint32_t>(_ping->intensities[i + 3]) << 24));
+    return v;
+  }
+
+  float intensity_float_log(const AzimuthRangeIndices &idx) const {
+    const auto intensity = read_uint32(idx);
+    const auto v =
+        log(static_cast<float>(std::max((uint)1, intensity)) / UINT32_MAX) *
+        10; // dbm
+
+    const auto min_db = (min_db_ == 0 ? log(1.0 / UINT32_MAX) * 10 : min_db_);
+
+    return std::min(1.0, std::max(0.0, (v - min_db) / range_db_));
   }
 
   bool do_log_scale_;
