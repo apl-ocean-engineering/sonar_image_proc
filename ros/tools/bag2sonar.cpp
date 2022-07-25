0ca45c9d178ec42167cd43dd5b93b3508775b876
Aaron Marburg <amarburg@uw.edu>
2022-07-25 15:17:17 -0700

    Initial commit of bag2sonar.cpp

---
+++ ros/tools/bag2sonar.cpp
@@ -0,0 +1,149 @@
+#include "sonar_image_proc/SonarDrawer.h"
+#include <acoustic_msgs/SonarImage.h>
+#include <boost/program_options.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core.hpp>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sonar_image_proc/sonar_image_msg_interface.h>
+#include <string>
+#include <vector>
+
+namespace po = boost::program_options;
+
+using std::string;
+using std::vector;
+
+class OutputWrapper {
+public:
+  virtual void write(const acoustic_msgs::SonarImage::ConstPtr &msg,
+                     const cv::Mat &mat) = 0;
+};
+
+class BagOutput : public OutputWrapper {
+public:
+  BagOutput(const std::string &bagfile, const std::string topic)
+      : _bag(bagfile, rosbag::bagmode::Write), _topic(topic) {}
+
+  void write(const acoustic_msgs::SonarImage::ConstPtr &msg,
+             const cv::Mat &mat) override {
+
+    cv_bridge::CvImage img_bridge(msg->header,
+                                  sensor_msgs::image_encodings::RGB8, mat);
+
+    sensor_msgs::Image output_msg;
+    img_bridge.toImageMsg(output_msg);
+
+    // Retain original message's timestamp
+    _bag.write(_topic, msg->header.stamp, output_msg);
+  }
+
+  rosbag::Bag _bag;
+  std::string _topic;
+};
+
+int main(int argc, char **argv) {
+  po::options_description description("Draw sonar from a bagfile");
+
+  // clang-format off
+  description.add_options()("help,h", "Display this help message")
+    ("output-topic,t", po::value<string>()->default_value("/drawn_sonar"), "Output topic")
+    ("logscale,l", po::bool_switch()->default_value(true), "do logscale")
+    ("min-db", po::value<float>()->default_value(0), "Min db")
+    ("max-db", po::value<float>()->default_value(0), "Max db")
+    ("osd", po::bool_switch()->default_value(true), "Include OSD in output")
+    ("output-bag,o", po::value<string>(), "output bagfile")
+    ("input-files", po::value<std::vector<std::string>>(), "Input files");
+  // clang-format on
+
+  po::positional_options_description p;
+  p.add("input-files", -1);
+
+  // try {
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+                .options(description)
+                .positional(p)
+                .run(),
+            vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << description;
+    exit(0);
+  } else if (vm.count("input-files")) {
+
+    if (vm.count("input-files") > 1) {
+      std::cerr << "Can only process one file at a time" << std::endl;
+      exit(-1);
+    }
+
+    std::vector<std::shared_ptr<OutputWrapper>> outputs;
+
+    if (vm.count("output-bag")) {
+      outputs.push_back(std::make_shared<BagOutput>(
+          vm["output-bag"].as<string>(), vm["output-topic"].as<string>()));
+    }
+
+    sonar_image_proc::SonarDrawer sonar_drawer;
+    std::unique_ptr<sonar_image_proc::SonarColorMap> color_map(
+        new sonar_image_proc::InfernoColorMap);
+
+    std::vector<std::string> files =
+        vm["input-files"].as<std::vector<std::string>>();
+    for (std::string file : files) {
+      std::cout << "Processing input file " << file << std::endl;
+
+      rosbag::Bag bag(file, rosbag::bagmode::Read);
+
+      std::cout << "Bagfile " << file << " is " << bag.getSize() << " bytes"
+                << std::endl;
+
+      rosbag::View view(bag, rosbag::TypeQuery("acoustic_msgs/SonarImage"));
+
+      int count = 0;
+
+      BOOST_FOREACH (rosbag::MessageInstance const m, view) {
+        acoustic_msgs::SonarImage::ConstPtr msg =
+            m.instantiate<acoustic_msgs::SonarImage>();
+
+        sonar_image_proc::SonarImageMsgInterface interface(msg);
+        if (vm["logscale"].as<bool>()) {
+          interface.do_log_scale(vm["min-db"].as<float>(),
+                                 vm["max-db"].as<float>());
+        }
+
+        cv::Mat rectMat =
+            sonar_drawer.drawRectSonarImage(interface, *color_map);
+
+        cv::Mat sonarMat = sonar_drawer.remapRectSonarImage(interface, rectMat);
+
+        cv::Mat outMat;
+        if (vm["osd"].as<bool>()) {
+          outMat = sonar_drawer.drawOverlay(interface, sonarMat);
+        } else {
+          outMat = sonarMat;
+        }
+
+        for (auto output : outputs) {
+          output->write(msg, outMat);
+        }
+
+        count++;
+
+        if (count % 100) {
+          std::cout << "Processed " << count << " sonar frames" << std::endl;
+        }
+      }
+
+      bag.close();
+    }
+  }
+
+  // } catch (const std::exception &e) {
+  //   std::cerr << e.what() << std::endl;
+  //   exit(-1);
+  // }
+
+  exit(0);
+}
